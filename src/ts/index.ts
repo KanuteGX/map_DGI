ac3900ae3b19442e0f691b00367829dcfbf2f2cb
KanuteGX <pimentel2002d@gmail.com>
2022-10-19 14:35:53 -0400

    map style pending

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,6 +2,10 @@ import { tileLayer, Map } from 'leaflet';
 
 const myMap = new Map('map').setView([51.505, -0.09], 8);
 
-tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+const mapUrl: string = `https://api.mapbox.com/styles/v1/illiantech/cl9fp0gvz001r14p7jtq6t6vl/tiles/512/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiaWxsaWFudGVjaCIsImEiOiJjbDlmcDZyYjE1NnY1M29teDN2eTh3dnF5In0.NhrvXnduOvE5mMbKFF07UA`;
+
+// const mapUrl: string = `https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`;
+
+tileLayer(mapUrl, {
 	maxZoom: 15
 }).addTo(myMap);
