5530973f49b13c35926ddffe0a2ec546e330cf98
KanuteGX <pimentel2002d@gmail.com>
2022-11-07 08:23:27 -0400

    map dgi

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -4,8 +4,6 @@ const myMap = new Map('map').setView([9.006826, -66.481284], 8);
 
 const mapUrl: string = `https://api.mapbox.com/styles/v1/illiantech/cl9hdcu23002314qsr5vfo2dg/tiles/512/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiaWxsaWFudGVjaCIsImEiOiJjbDlmcDZyYjE1NnY1M29teDN2eTh3dnF5In0.NhrvXnduOvE5mMbKFF07UA`;
 
-// const mapUrl: string = `https://tile.openstreetmap.org/{z}/{x}/{y}.png`;
-
 tileLayer(mapUrl, {
 	maxZoom: 15
 }).addTo(myMap);
